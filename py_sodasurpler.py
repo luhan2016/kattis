b341aff69f3678110c7269172d636779c8fdc693
lu <luhansophia@gmail.com>
2019-02-23 08:43:43 +0100

    Create py_sodasurpler.py

---
+++ py_sodasurpler.py
@@ -0,0 +1,28 @@
+//https://open.kattis.com/problems/sodasurpler
+
+line = input()
+data = line.split(' ')
+num = [int(i) for i in data]
+e = num[0]
+f = num[1]
+c = num[2]
+count=0
+while(e+f>=c):
+    e=int((e+f)/c)
+    f=(e+f)%c
+    count = count+e 
+    print(count)
+print(int(count))
+
+
+
+import sys
+import math
+
+(e, f, c) = map(int, sys.stdin.readline().split())
+bots = e + f
+drunk = 0
+while(bots/c >= 1):	
+	drunk = drunk + math.floor(bots/c)
+	bots = bots % c + math.floor(bots/c)
+print(drunk)
